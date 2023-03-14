4ccce9eec9846f613cc559ab03ee8cb0cb1b33bd
junior-slv <dirceu.junior@edu.unifil.br>
2023-03-14 18:00:43 -0300

    initial files

---
+++ Client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GoogleLogin from 'react-google-login'
+ 
+
+const App = () => {
+  return (
+    <div className="container">
+
+    </div>
+  )
+}
+
+export default App
